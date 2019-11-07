c80cd160a41a1bc0bb93cd8f338b3a0f1271724b
John Jardine <john@gprime.net>
2019-11-06 21:27:07 -0500

    Update stl-checks.sh

--- scripts/stl-checks.sh
+++ scripts/stl-checks.sh
@@ -3,11 +3,13 @@
 stl_path="${@:1}"
 result="$(./admesh "${stl_path}")"
 
+SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+
 echo "Validating integrity of ${stl_path}"
 
-echo "${result}" | ./stl-violation-check.sh 0 Backwards edges
-echo "${result}" | ./stl-violation-check.sh 0 Degenerate facets
-echo "${result}" | ./stl-violation-check.sh 0 Facets removed
-echo "${result}" | ./stl-violation-check.sh 0 Facets added
-echo "${result}" | ./stl-violation-check.sh 0 Facets reversed
-echo "${result}" | ./stl-violation-check.sh 0 Backwards edges
+echo "${result}" | ${SCRIPT_DIR}/stl-violation-check.sh 0 Backwards edges
+echo "${result}" | ${SCRIPT_DIR}/stl-violation-check.sh 0 Degenerate facets
+echo "${result}" | ${SCRIPT_DIR}/stl-violation-check.sh 0 Facets removed
+echo "${result}" | ${SCRIPT_DIR}/stl-violation-check.sh 0 Facets added
+echo "${result}" | ${SCRIPT_DIR}/stl-violation-check.sh 0 Facets reversed
+echo "${result}" | ${SCRIPT_DIR}/stl-violation-check.sh 0 Backwards edges
