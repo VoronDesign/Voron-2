588ae33f4984987ee469aac1998830379336aa38
John Jardine <john@gprime.net>
2019-11-06 20:02:16 -0500

    Update travis-build.sh

--- scripts/travis-build.sh
+++ scripts/travis-build.sh
@@ -1,1 +1,9 @@
-echo "Test"
+CACHE_DIR=${PWD}/travis_cache
+mkdir -p CACHE_DIR
+cd CACHE_DIR
+wget https://github.com/admesh/admesh/releases/download/v0.98.4/admesh-0.98.4.tar.gz
+tar -zxvf admesh-0.98.4.tar.gz
+cd admesh-0.98.4
+./configure
+make
+./admesh --help
