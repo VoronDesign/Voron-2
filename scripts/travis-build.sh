156c168e26f1ad9ea7513b5bb494e9e025988a1b
John Jardine <john@gprime.net>
2019-11-06 20:12:24 -0500

    Update travis-build.sh

--- scripts/travis-build.sh
+++ scripts/travis-build.sh
@@ -1,9 +1,14 @@
+BASE_DIR=${PWD}
 CACHE_DIR=${PWD}/travis_cache
+ADMESH_DIR=${CACHE_DIR}/admesh-0.98.4
 mkdir -p CACHE_DIR
 cd CACHE_DIR
-wget https://github.com/admesh/admesh/releases/download/v0.98.4/admesh-0.98.4.tar.gz
-tar -zxvf admesh-0.98.4.tar.gz
-cd admesh-0.98.4
-./configure
-make
-./admesh --help
+if [ ! -d ADMESH_DIR ]; then
+  wget https://github.com/admesh/admesh/releases/download/v0.98.4/admesh-0.98.4.tar.gz
+  tar -zxvf admesh-0.98.4.tar.gz
+  cd ADMESH_DIR
+  ./configure
+  make
+fi
+cd ADMESH_DIR
+find BASE_DIR -type f -iname "*.STL" -exec admesh {} \;
