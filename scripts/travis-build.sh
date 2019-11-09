11cfc33ec7dbaa5cee035c123b0498a983a2cd61
John Jardine <john@gprime.net>
2019-11-08 19:56:22 -0500

    Update travis-build.sh

--- scripts/travis-build.sh
+++ scripts/travis-build.sh
@@ -3,17 +3,6 @@ CACHE_DIR=${PWD}/travis_cache
 
 chmod +x ${BASE_DIR}/scripts/*.py
 
-ADMESH_DIR=${CACHE_DIR}/admesh-0.98.4
-mkdir -p ${CACHE_DIR}
-cd ${CACHE_DIR}
-if [ ! -d ${ADMESH_DIR} ]; then
-  echo "Admesh cache miss; fetching and building ..."
-  wget https://github.com/admesh/admesh/releases/download/v0.98.4/admesh-0.98.4.tar.gz
-  tar -zxvf admesh-0.98.4.tar.gz
-  cd ${ADMESH_DIR}
-  ./configure
-  make
-  chmod +x admesh
-fi
-cd ${ADMESH_DIR}
-${BASE_DIR}/scripts/validate-stls.py ${BASE_DIR}
+git diff --name-only --diff-filter=AM HEAD...$TRAVIS_BRANCH
+
+
