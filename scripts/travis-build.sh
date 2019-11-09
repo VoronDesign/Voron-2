0947e539325a9d632a0894653376cd731d5d3f1b
John Jardine <john@gprime.net>
2019-11-08 21:10:15 -0500

    Update travis-build.sh

--- scripts/travis-build.sh
+++ scripts/travis-build.sh
@@ -1,25 +1,3 @@
-#!/bin/bash
-
-BASE_DIR=${PWD}
-CACHE_DIR=${PWD}/travis_cache
-
-chmod +x ${BASE_DIR}/scripts/*.py
-
-ADMESH_DIR=${CACHE_DIR}/admesh-0.98.4
-mkdir -p ${CACHE_DIR}
-cd ${CACHE_DIR}
-if [ ! -d ${ADMESH_DIR} ]; then
-  echo "Admesh cache miss; fetching and building ..."
-  wget https://github.com/admesh/admesh/releases/download/v0.98.4/admesh-0.98.4.tar.gz
-  tar -zxvf admesh-0.98.4.tar.gz
-  cd ${ADMESH_DIR}
-  ./configure
-  make
-  chmod +x admesh
-fi
-cd ${ADMESH_DIR}
-
-# Find changed files
-git remote set-branches --add origin master
+git config remote.origin.fetch "+refs/heads/*:refs/remotes/origin/*"
 git fetch
-git diff --name-only --diff-filter=AMR origin/master | xargs -n 1 -I {} ${BASE_DIR}/scripts/validate-file.py ${BASE_DIR}/{}
+git diff --name-only HEAD origin/${TRAVIS_BRANCH}
